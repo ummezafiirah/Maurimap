735773fdccd9dc0fb6fcac344da5788e890065b7
ummezafiirah <40118799+ummezafiirah@users.noreply.github.com>
2019-02-02 00:17:37 +0400

    Update search.py

--- SearchAPI/search.py
+++ SearchAPI/search.py
@@ -1,12 +1,11 @@
 from geopy.geocoders import Nominatim
-from analysis import text_analyse
+from analysis import text_analysetwitter
 import string
 import pymongo
-myclient = pymongo.MongoClient('mongodb://localhost:27017/')
-#print(myclient.list_database_names())
-mydb = myclient['myproject']
-mycol = mydb["tweets"]
-
+import re
+import datetime
+#import connect_db to connect to cloud
+from seachAPI import connect_db
 geolocator = Nominatim(user_agent="my-application")
 # Import the necessary package to process data in JSON format
 try:
@@ -14,12 +13,14 @@ try:
 except ImportError:
     import simplejson as json
 
-# Import the tweepy library
+#Import the tweepy library
 import tweepy
 import pymongo
-myclient = pymongo.MongoClient('mongodb://localhost:27017/')
-db = myclient['twitter_db']
-collection = db['twitter_collection']
+
+#to connect to local mongo db
+#myclient = pymongo.MongoClient('mongodb://localhost:27017/')
+#db = myclient['twitter_db']
+#collection = db['twitter_collection']
 
 
 # Variables that contains the user credentials to access Twitter API
@@ -39,11 +40,12 @@ api = tweepy.API(auth,wait_on_rate_limit=True)
 #Use csv Writer
 #csvWriter = csv.writer(csvFile)
 
-query = 'place:ee9d992aa12a6fa0  influenza OR gotflu OR coughing OR cough OR caughtcold ' \
-        'OR gastro OR toux OR gastroenteritis OR gas OR diarrhea OR gotgastro OR conjunctivitis' \
-        'éternuements OR sneezing OR conjonctivite OR eyeswelling OR flu OR fièvre OR grippe OR eyeitching' \
-        'diarrhée OR vomissement OR vertiges OR stomachpain OR cramps OR dizziness OR sorethroat OR headache' \
-        'fever OR vomiting OR eyetearing OR runnynose OR gastro-entérite OR déshydratation OR rougeurdesyeux'
+#keywords to find in tweets
+query = 'place:ee9d992aa12a6fa0  flu OR influenza OR coughing OR cough ' \
+        'OR gastro OR toux OR gastroenteritis OR diarrhea OR conjunctivitis' \
+        'éternuements OR sneezing OR conjonctivite OR pinkEye OR eyeswelling OR fièvre OR grippe OR eyeitching' \
+        'diarrhée OR vomissement OR vertiges OR cramps OR crampes OR dizziness' \
+        'fever OR vomiting OR eyetearing OR gastro-entérite OR respiratoryInfection OR infectionRespiratoire'
 
 
 for status in tweepy.Cursor(api.search,q=query\
@@ -52,44 +54,88 @@ for status in tweepy.Cursor(api.search,q=query\
      #print (status.created_at)
      #print("Tweet Text:"+status.text)
      #print("Tweet place:"+status.place.name if status.place else "Undefined place")
+
+     # if location(coordinates) are available >>search for location in tweet
      if (status.geo is None):
-         location_result = text_analyse.extract_location(status.text ,80)
+         location_result = text_analysetwitter.extract_location(status.text.lower() ,60)
+         dis = text_analysetwitter.extract_disease(status.text, 60)
+
 
          #replacing punctuations with whitespace for location_result
          for char in string.punctuation:
             s = location_result.replace(char, ' ')
+            s1 = dis.replace(char, ' ')
          #removing numbers from s
-         result = ''.join([i for i in s if not i.isdigit()])
+         locresult = ''.join([i for i in s if not i.isdigit()])
+         disresult = ''.join([i for i in s1 if not i.isdigit()])
+         #print("disease removing special characters:"+re.sub('[^A-Za-z0-9]+', '', disresult))
 
-         location = geolocator.geocode(result+", Mauritius")
-         lat = location.latitude
-         long = location.longitude
-         print(location.address)
-         print((location.latitude, location.longitude))
+         #condition to discard tweets with no location mentionned in it
+         if locresult != "":
+            #example output: return value('dizziness', 100),influenza
+            a,b,c= dis.split(',',3)
+            #('dizziness'
+            #print(a)
+            #100)
+            #print(b)
+            #influenza
+            print("Disease:" + c)
+            disease = c
 
-         if (mycol.find_one({"date": status.created_at,"Tweet": status.text}) is None):
-             #insertinMongoDB
-             mylist = { "date":(status.created_at),"Tweet": (status.text), "address": (location.address), "latitude":(location.latitude), "longitude":(location.longitude)}
-             x = mycol.insert_one(mylist)
+            location = geolocator.geocode(locresult+", Mauritius")
+            lat = location.latitude
+            long = location.longitude
+            #print(location.address)
+            #print((location.latitude, location.longitude))
+            date = str(status.created_at.date())
+            print(date)
+            # if (mycol.find_one({"date": status.created_at,"Tweet": status.text}) is None):
+             #insertinMongoDBCloud
+            #,"location":(location.address)
+            mylist = { "diseasetype":(disease),"date":(date),"Tweet": (status.text), "latitude":(location.latitude), "longitude":(location.longitude)}
+            connect_db.save_tweet(mylist)
+            ##for Local Mongo db
+            ##save_data(myList, 'twitter')
+            ##x = mycol.insert_one(mylist)
 
+     #if location(coordinates) are available
      else:
-         if (mycol.find_one({"date": status.created_at, "Tweet": status.text}) is None):
+        # if (mycol.find_one({"date": status.created_at, "Tweet": status.text}) is None):
              # insertinMongoDB
+             dis = text_analysetwitter.extract_disease(status.text.lower(), 60)
+        # replacing punctuations with whitespace for location_result
+             for char in string.punctuation:
+                s = location_result.replace(char, ' ')
+                s1 = dis.replace(char, ' ')
+                # removing numbers from s
+                locresult = ''.join([i for i in s if not i.isdigit()])
+                disresult = ''.join([i for i in s1 if not i.isdigit()])
+
+             # example output: return value('dizziness', 100),influenza
+             a, b, c = dis.split(',', 3)
+             # ('dizziness'
+             #print(a)
+             # 100)
+             #print(b)
+             # influenza
+             print("Disease:" + c)
+             disease = c
+
              geo = status.geo.get("coordinates")
              lat = geo[0]
              long = geo[1]
              coordinates = lat, long
-             print(coordinates)
+             ##print(coordinates)
              location = geolocator.reverse(coordinates)
-             print(location)
-
+             ##print(location)
+             date = str(status.created_at.date())
+             print(date)
              ##reverse
              location = geolocator.reverse(coordinates)
-             mylist = {"date": (status.created_at), "Tweet": (status.text), "address": (location),
+             mylist = {"diseasetype":(disease),"date": (date), "Tweet": (status.text),
                        "latitude": (lat), "longitude": (long)}
-             x = mycol.insert_one(mylist)
+             connect_db.save_tweet(mylist)
 
+             ##for Local Mongo db
+             # x = mycol.insert_one(mylist)
 
-
-#for x in mycol.find({}, {"_id": 0, "date": 1, "Tweet": 1}):
- #   print(x)
