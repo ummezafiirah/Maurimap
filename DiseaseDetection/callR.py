257d36d785a7568f0d174eeb2621af316ba7800a
ummezafiirah <40118799+ummezafiirah@users.noreply.github.com>
2019-03-28 11:52:30 +0400

    Update callR.py

--- DiseaseDetection/callR.py
+++ DiseaseDetection/callR.py
@@ -2,5 +2,5 @@
 #subprocess.call("C:/Users/zafiirah/PycharmProjects/Rprogramming/detection_disease.R", shell=True)
 import rpy2.robjects as robjects
 r_source = robjects.r['source']
-r_source("C:/Users/zafiirah/PycharmProjects/Rprogramming/detection_disease.R")
+r_source("C:/Users/zafiirah/PycharmProjects/Rprogramming/disease_detection.R")
 print ('r script finished running')
